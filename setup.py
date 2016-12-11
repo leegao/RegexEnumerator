9740d8fb3ea46df74e49b16f0c821e4d366d1cd7
leegao <lg342@cornell.edu>
2016-12-10 23:16:32 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='regex_enumerate',
+      version='0.0.1a0',
+      description='Enumerate Regular Expressions',
+      url='http://github.com/leegao/RegexEnumerator',
+      author='Lee Gao',
+      author_email='lg342@cornell.edu',
+      license='MIT',
+      packages=['regex_enumerate'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords='regular expression combinatorics',
+      install_requires=[
+          'sympy',
+          'numpy',
+          'pyoeis'
+      ],
+      include_package_data=True,
+      zip_safe=False)
