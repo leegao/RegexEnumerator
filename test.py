216b0e1f35f2ccbe1df6f761a47be483f7e7e7f5
Lee Gao <lg342@cornell.edu>
2016-12-10 19:58:56 -0800

    typos

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from regex_enumerate.transfer import *
 regexes = [
     ("(00*1)*", '1-separated strings that starts with 0 and ends with 1'),
     ("(%|1|11)(00*(1|11))*0* | 1", 'complete 1 or 11-separated strings'),
-    ("(000)*(111)*(22)*(33)*(44)*", 'complex root to (1 - z**3)**-2 * (1 - z**2)**-3'),
+    ("(000)*(111)*(22)*(33)*(44)*", r'complex root to $\frac{1}{(1 - z^3)^2} * \frac{1}{(1 - z^2)^3}$'),
     ("1*(22)*(333)*(4444)*(55555)*",  'number of ways to make change give coins of denomination 1 2 3 4 and 5'),
     (' '.join("%s%s*" % (i, i) for i in range(1, 6)), '5 compositions of n'),
     ("(11*)*",  'all compositions of n'),
@@ -14,9 +14,9 @@ regexes = [
     ("(00*1)*00*", 'list of 0-sequences'),
 ]
 for regex, comment in regexes:
-    print("* `%s`: %s" % (regex, comment))
+    print("* `%s`: %s\n" % (regex, comment))
     algebraic = list(islice(map(lambda x: int(round(x)), enumerate_coefficients(regex)), 20))
-    print("  Its generating function is\n$$\n  %s\n  $$" % latex(generating_function(regex)))
+    print("  Its generating function is\n  $$\n  %s\n  $$" % latex(generating_function(regex)))
     print("  For words of sizes up to 20 in this language, their counts are:\n\n      %s\n" % (', '.join(map(str, algebraic))))
-    print("  Its closed form is\n$$\n  %s\n  $$" % latex(algebraic_form(regex)))
+    print("  Its closed form is\n  $$\n  %s\n  $$" % latex(algebraic_form(regex)))
     print()
